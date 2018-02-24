4b423adb6c0e710b8c0e306fe92fac5b032393ee
mahirjain25 <mahirjain25@gmail.com>
2018-02-24 16:22:32 +0530

    Added some system calls

---
+++ routes/executables/system_calls/open.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+// Usage ./open path
+int main(int argc, char const *argv[])
+{
+	if(strcmp(argv[0],"./open")==0)
+		{
+			int filedescriptor = open(argv[1],O_CREAT | O_RDWR,S_IRWXU);
+			if(filedescriptor<0)
+				perror("Error");
+			else
+				printf("File opened\n");
+		}
+
+}
