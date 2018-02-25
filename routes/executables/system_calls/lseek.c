4c84697e5f1973e7d7f369862ff7dd7bc3503b3c
mahirjain25 <mahirjain25@gmail.com>
2018-02-25 10:35:29 +0530

    Added lseek,read calls.

---
+++ routes/executables/system_calls/lseek.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Usage ./lseek offset flag path
+
+
+int main(int argc, char const *argv[])
+{	int filedescriptor = open(argv[3],O_RDWR,S_IRWXU);
+	int a;
+	if(filedescriptor <0)
+	perror("Error ");
+	
+	if(strcmp(argv[0],"./lseek")==0 && strcmp(argv[2],"SEEK_END")==0)
+	{	a = lseek(filedescriptor,atoi(argv[1]),SEEK_END);
+		if(a<0)
+		perror("Error ");
+		else
+		printf("Position of file pointer set to: %d\n",a);
+	}
+	else if(strcmp(argv[0],"./lseek")==0 && strcmp(argv[2],"SEEK_CUR")==0)
+	{
+		a = lseek(filedescriptor,atoi(argv[1]),SEEK_CUR);
+		if(a<0)
+		perror("Error ");
+		else
+		printf("Position of file pointer set to: %d\n",a);
+	}
+	else if(strcmp(argv[0],"./lseek")==0 && strcmp(argv[2],"SEEK_SET")==0)
+	{
+		a = lseek(filedescriptor,atoi(argv[1]),SEEK_SET);
+		if(a<0)
+		perror("Error ");
+		else
+		printf("Position of file pointer set to: %d\n",a);
+	}
+}
