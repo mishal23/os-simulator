0ee7b0231ea03006fd27a985b0ac9ea32c034b16
Mishal Shah <shahmishal1998@gmail.com>
2018-03-06 21:56:48 +0530

    Socket Programming simulation added

--- public/javascripts/socket_programming/socket_programming.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-	var topic = "Socket Programming";
-    $('#side_top_navbar').load('../base.html', function(){
-    	$('.left').html(topic);
-    });
-});
---
+++ public/javascripts/socket_programming/tcp_ip.js
@@ -0,0 +1,112 @@
+$(document).ready(function() {
+	$('.test').show();
+	var topic = "TCP/IP Simulation";
+    $('#side_top_navbar').load('../base.html', function(){
+    	$('.left').html(topic);
+    });
+
+    $(".macbook-content").css("background-color","#000");
+    $("#server").hide();
+    $(".macbook1-content").css("background-color","#000");
+
+    $("#client").hide();
+    $(".wifi1").hide();
+    $(".wifi2").hide();
+    $(".fa-telegram").hide();
+
+    // hide all buttons except for socket intially
+    $("#bind").hide();
+    $("#listen").hide();
+    $("#connect").hide();
+    $("#accept").hide();
+    $("#send").hide();
+    $("#receive").hide();
+    $("#close").hide();
+
+    var string = "";
+    string+="10.10.100.80<br>";
+
+    $("#socket").click(function(){
+    	$("#server").show();
+    	$(".macbook-content").css("background-color","#fff");
+
+    	$("#client").show();
+    	$(".macbook1-content").css("background-color","#fff");
+
+    	$("#bind").show();
+    });
+    $("#bind").click(function () {
+    	$("#ip").typed({
+		    strings: [
+		      string
+		    ],
+		    typeSpeed: 0,
+		 });
+    	$("#listen").show();
+    });
+    $("#listen").click(function () {
+    	$(".wifi1").show();
+    	$(".wifi1").addClass("blink");
+
+    	$("#connect").show();
+    });
+    $("#connect").click(function () {
+    	$(".wifi2").show();
+    	$(".wifi2").addClass("blink");
+
+    	$("#accept").show();
+    });
+    $("#accept").click(function () {
+    	
+    	$(".wifi1").removeClass("blink");
+    	$(".wifi2").removeClass("blink");
+
+    	$("#send").show();
+    });
+    
+    $("#send").click(function () {
+    	$(".wifi1").hide();
+    	$(".wifi2").hide();
+    	$(".fa-telegram").show();
+    	$(".fa-telegram").addClass("send-sim");
+    	var client_message = "Message received from server"
+    	setTimeout(function(){
+    		$("#client-message").typed({
+		    strings: [
+		      client_message
+		    ],
+		    typeSpeed: 0,
+		 });
+    	}, 2300);	
+    	
+
+    	$("#receive").show();
+    });
+    $("#receive").click(function () {
+    	$(".fa-telegram").show();
+    	$(".fa-telegram").removeClass("send-sim");
+    	$(".fa-telegram").addClass("receive-sim");
+    	$("#close").show();
+
+    	var server_message = "Message received from client"
+    	setTimeout(function(){
+    		$("#server-message").typed({
+		    strings: [
+		      server_message
+		    ],
+		    typeSpeed: 0,
+		 });
+    	}, 2500);	
+    	
+    });
+    $("#close").click(function () {
+	    $(".macbook-content").css("background-color","#000");
+	    $("#server").hide();
+	    $("#client").hide();
+	    $("#ip").hide();
+	    $(".macbook1-content").css("background-color","#000");
+
+	    $("#server-message").hide();
+	    $("#client-message").hide();
+    });
+});
