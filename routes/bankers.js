5497cd3066aa128dbbafc1639661255ce2b42f35
Mishal Shah <shahmishal1998@gmail.com>
2018-03-09 19:24:36 +0530

    bankers backend added

--- routes/bankers.js
+++ routes/bankers.js
@@ -4,12 +4,32 @@ var router = express.Router();
 
 // Safe Sequence call
 router.post('/safe_sequence', function(req,res){
-
+	var input = "";
+	for(var i=0;i<req.body.length;i++)
+	{
+		input+=req.body[i]+" ";
+	}
+	console.log(input);
+	
+	exec(__dirname + '/executables/bankers/bankers_safe_sequence ' + input , function callback(error,stdout,stderr) {
+		console.log(error);
+		res.send(stdout);
+	});
 });
 
 // Resource Request call
 router.post('/resource_request', function(req,res){
-
+	var input = "";
+	for(var i=0;i<req.body.length;i++)
+	{
+		input+=req.body[i]+" ";
+	}
+	console.log(input);
+	
+	exec(__dirname + '/executables/bankers/bankers_resource_request ' + input , function callback(error,stdout,stderr) {
+		console.log(error);
+		res.send(stdout);
+	});
 });
 
 module.exports = router;
