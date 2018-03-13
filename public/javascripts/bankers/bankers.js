2d1efe384bc1d2898e501dbeadbd805d501bf867
m0bi5 <mohitbhasi1998@gmail.com>
2018-03-12 18:52:12 -0700

    Bankers safe sequence done

--- public/javascripts/bankers/bankers.js
+++ public/javascripts/bankers/bankers.js
@@ -6,12 +6,13 @@ $(document).ready(function() {
     $('#b1').click(function(){
     	var np=$('#np').val();
     	var nr=$('#nr').val();
+        document.getElementById("c2").innerHTML=""
     	var i=0;
     	var j=0;
     	$('#c2').append('<span class="text-center">Enter current allocation matrix</span><br><br>');
     	for(i=0;i<np;i+=1)
     	{
-    		$('#c2').append('<span id="ap'+i+'">P'+i+'</span>');
+    		$('#c2').append('<div id="ap'+i+'">P'+i+'</div><br>');
     		for(j=0;j<nr;j+=1)
     		{
     			$('#ap'+i).append('<input style="margin-right:5px;" placeholder="R'+j+'"class="col-md-3" id="ap'+i+'ar'+j+'"></input>');
@@ -19,16 +20,18 @@ $(document).ready(function() {
     		$('#c2').append('<br><br>');
     	}
     	$('#2').fadeIn();
+        $("b1").attr("disabled", "disabled");
   	});
     $('#b2').click(function(){
     	var np=$('#np').val();
     	var nr=$('#nr').val();
     	var i=0;
     	var j=0;
+        document.getElementById("c3").innerHTML=""
     	$('#c3').append('<span class="text-center">Enter maximum allocation matrix</span><br><br>');
     	for(i=0;i<np;i+=1)
     	{
-    		$('#c3').append('<span id="mp'+i+'">P'+i+'</span>');
+    		$('#c3').append('<div id="mp'+i+'">P'+i+'</div><br>');
     		for(j=0;j<nr;j+=1)
     		{
     			$('#mp'+i).append('<input style="margin-right:5px;" placeholder="R'+j+'"class="col-md-3" id="mp'+i+'mr'+j+'"></input>');
@@ -36,10 +39,12 @@ $(document).ready(function() {
     		$('#c3').append('<br><br>');
     	}
     	$('#3').fadeIn();
+        $("b2").attr("disabled", "disabled");
   	});
     $('#b3').click(function(){
     	var nr=$('#nr').val();
     	var i=0;
+        document.getElementById("c4").innerHTML=""
     	$('#c4').append('<span class="text-center">Enter available resources of each type <span><br><br>');
     	for(i=0;i<nr;i+=1)
     	{
@@ -47,12 +52,16 @@ $(document).ready(function() {
     	}
     	$('#4').fadeIn();
     	$('#action').fadeIn();
+        $("b3").attr("disabled", "disabled");
   	});
 
 
     $('#action').click(function(){
     	var np=$('#np').val();
     	var nr=$('#nr').val();
+        document.getElementById("result").innerHTML="";
+        document.getElementById("is").innerHTML="";
+        document.getElementById("os").innerHTML="";
     	var i=0;
     	var j=0;
     	arguments=np+' '+nr+' ';
@@ -79,24 +88,55 @@ $(document).ready(function() {
 		//arguments=arguments.split(' ');
 		for(var i=0;i<arguments.length-1;i+=1)
 			req[i]=parseInt(arguments[i]);
+        var result;
 
 		$.ajax({
 			type: 'POST',
 			data: {arguments},
             url: 'http://localhost:3000/bankers/safe_sequence',						
             success: function(data) {
+                setTimeout(function() {
+                  $('.spec').addClass('fa-spin');
+               }, 4);         
+                var out=[],result;
+                $('#is').animate({opacity: 1}, 4);  
                 console.log('success');
                 $("#result").append(data);
+                //$('.fa-cog').addClass('fa-spin');
+                result=data
+                for(j=0;j<np;j+=1)
+                {
+                    $('#is').append('<span class="anip" style="margin-right:10px;padding: 3px;border: solid 1px black;">P'+j+'</span>');
+                }
+           
+                $('#is').animate({opacity: 0}, 4000);    
+                j=0;
+                i=0;
+                if(result[0]=='S')
+                {
+                    for(j in result)
+                        if(result[j]>='0' && result[j]<='9')
+                        {
+                            out[i]=result[j];
+                            i+=1;
+                        }
+
+                    for(j=0;j<np;j+=1)
+                    {
+                        $('#os').append('<span class="anip" style="margin:10px;padding: 3px;border: solid 1px black;">P'+out[j]+'</span>');
+                    }
+                }
+                else
+                {
+                     $('#os').append('<span>No safe sequence possible</span>')
+                }
+           
+                $('#os').hide().delay(6000).fadeIn();    
+               setTimeout(function() {
+                  $('.spec').removeClass('fa-spin');
+               }, 6000);                
             }
         });
-		for(j=0;j<np;j+=1)
-		{
-			$('#is').append('<span class="anip" style="margin-right:10px;padding: 3px;border: solid 1px black;">P'+j+'</span>');
-		}
-   
-      	$('.fa-spin').show().delay(4000).fadeOut();
-      	$('#cog1').hide().delay(5000).fadeIn();
-      	$('#cog2').hide().delay(5000).fadeIn();
-      	$('.anip').show().delay(4000).fadeOut();
+	
     });
 });
