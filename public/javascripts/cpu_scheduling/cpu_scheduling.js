87dadc73ca2bdc3653c3c472e9b23b7227f49e14
palak singhal <smarty1palak@gmail.com>
2018-03-30 02:11:40 -0700

    Cpu scheduling (#32)
    * Graph for comparison of scheduling algorithms, modified C code
    
    * some changes to frontend
    
    * Ouput tables added
    
    * added completion time
    
    * errors in executables corrected and removed side and top nav bar

--- public/javascripts/cpu_scheduling/cpu_scheduling.js
+++ public/javascripts/cpu_scheduling/cpu_scheduling.js
@@ -1,6 +1,255 @@
 $(document).ready(function() {
-	var topic = "CPU Scheduling";
+    var topic = "CPU Scheduling";
     $('#side_top_navbar').load('../base.html', function () {
-    	$('.left').html(topic);
+        $('.left').html(topic);
     })
 });
+
+var n_processes = 1;
+//Function to select HTML Object by ID
+function S(id){
+    return document.getElementById(id)
+}
+function put_processes(){
+    processes = S('processes');
+    processes.innerHTML = "";
+    for(var i=0;i<n_processes;i++){
+        var p = '<div class="row"> <div class="input-field col-sm-4"> <input class="validate" type="text" id="p_at'+i+'"/> <label for="head_pos">Arrival Time</label> </div> <div class="input-field col-sm-4"> <input class="validate" type="text" id="p_bt'+i+'"/> <label for="head_pos">Burst Time</label> </div> <div class="input-field col-sm-4"> <input class="validate" type="text" id="p_pt'+i+'"/> <label for="head_pos">Priority</label> </div> </div>';
+        processes.innerHTML += p;
+    }
+}
+//To add Process
+function add_process(){
+    n_processes = S('n_processes').innerHTML;
+    S('n_processes').innerHTML = ++n_processes;
+    put_processes();
+}
+function remove_process(){
+    if(n_processes === 1)
+        return;
+    n_processes = S('n_processes').innerHTML;
+    S('n_processes').innerHTML = --n_processes;
+    put_processes();
+}
+
+function submit() {
+    var tq = $("#time_quantum").value;
+    console.log("no of process : "+n_processes);
+    var input = n_processes + ' ';
+    var input_p = n_processes + ' ';
+    var input_rr;
+    var output =  [];
+    var tt = [], wt = [];
+    for(var i = 0, j = 0; i < 3*n_processes; i+=3, j++) {
+        var at = S('p_at'+j).value,
+            bt = S('p_bt'+j).value,
+            pt = S('p_pt'+j).value;
+        input += (j+1) + ' ' + at + ' ' + bt + ' ';
+        input_p += (j+1) + ' ' + at + ' ' + bt + ' ' + pt + ' ';
+    }
+    input_rr = input + tq;
+    console.log(input);
+
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:3000/cpu_scheduling/fcfs",
+        data: {input : input},
+        success: function(result){
+            console.log(result);
+            output[0] = result;
+            result = result.split('\n');
+            var len = result.length;
+            tt[0] = result[len-1];
+            wt[0] = result[len-2];
+        },
+        async: false
+    });
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:3000/cpu_scheduling/sjf-nonpreemptive",
+        data: {input : input},
+        success: function(result){
+            console.log(result);
+
+            output[1] = result;
+            result = result.split('\n');
+            var len = result.length;
+            tt[1] = result[len-1];
+            wt[1] = result[len-2];
+
+        },
+        async: false
+    });
+
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:3000/cpu_scheduling/sjf-preemptive",
+        data: {input : input},
+        success: function(result){
+            console.log(result);
+
+            output[2] = result;
+            result = result.split('\n');
+            var len = result.length;
+            tt[2] = result[len-1];
+            wt[2] = result[len-2];
+
+        },
+        async: false
+    });
+
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:3000/cpu_scheduling/priority-nonpreemptive",
+        data: {input : input_p},
+        success: function(result){
+            console.log(result);
+
+            output[3] = result;
+            result = result.split('\n');
+            var len = result.length;
+            tt[3] = result[len-1];
+            wt[3] = result[len-2];
+
+        },
+        async: false
+    });
+
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:3000/cpu_scheduling/priority-preemptive",
+        data: {input : input_p},
+        success: function(result){
+            console.log(result);
+
+            output[4] = result;
+            result = result.split('\n');
+            var len = result.length;
+            tt[4] = result[len-1];
+            wt[4] = result[len-2];
+
+        },
+        async: false
+    });
+
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:3000/cpu_scheduling/priority-aging",
+        data: {input : input_p},
+        success: function(result){
+            console.log(result);
+
+            output[5] = result;
+            result = result.split('\n');
+            var len = result.length;
+            tt[5] = result[len-1];
+            wt[5] = result[len-2];
+
+        },
+        async: false
+    });
+
+    $.ajax({
+        type: "POST",
+        url: "http://localhost:3000/cpu_scheduling/round_robin",
+        data: {input : input_rr},
+        success: function(result){
+            console.log(result);
+
+            output[6] = result;
+            result = result.split('\n');
+            var len = result.length;
+            tt[6] = result[len-1];
+            wt[6] = result[len-2];
+
+        },
+        async: false
+    });
+    print_table(output);
+    draw_graph(tt, wt);
+
+}
+
+function to_float(output){
+    for(var i=0;i<output.length;i++)
+        output[i] = parseFloat(output[i]);
+    return output;
+}
+
+function draw_graph(tt, wt) {
+    var algo = ['FCFS','SJF Non-preemptive', 'SJF Preemptive', 'Priority Non-preemptive', 'Priority Preemptive', 'Priority Aging', 'Round Robin'];
+    tt = to_float(tt);
+    wt = to_float(wt);
+    console.log(tt);
+    console.log(wt);
+    $("body").append('<div class="container-fluid"><div class="row"><div class="col-sm-12" id="chart-container"></div></div></div>');
+    var chart = {
+        type: 'bar'
+    };
+    var title = {
+        text: 'Comparision of Process Scheduling Algorithms'
+    };
+    var xAxis = {
+        categories: algo
+    };
+    var yAxis = {
+        min: 0,
+        title: {
+            text: 'Time',
+            align: 'high'
+        },
+        labels: {
+            overflow: 'justify'
+        }
+    };
+    var series= [{
+        name: 'Turn Around Time',
+        data: tt
+    }, {
+        name: 'Waiting Time',
+        data: wt
+    }
+    ];
+    var plotOptions =  {
+        series: {
+            pointWidth: 10
+        }
+    };
+
+    var json = {};
+    json.chart = chart;
+    json.title = title;
+    //json.tooltip = tooltip;
+    json.xAxis = xAxis;
+    json.yAxis = yAxis;
+    json.series = series;
+    json.plotOptions = plotOptions;
+    //json.legend = legend;
+    //json.credits = credits;
+    $('#chart-container').highcharts(json);
+}
+
+function print_table(output) {
+    console.log(output);
+    var algo = ['FCFS','SJF Non-preemptive', 'SJF Preemptive', 'Priority Non-preemptive', 'Priority Preemptive', 'Priority Aging', 'Round Robin'];
+    $("body").append("<div class='container-fluid' id='output'></div>");
+    var op = $('#output');
+
+    for(var i = 0; i < output.length; ++i) {
+        op.append("<div class='row'><div class='col-sm-8 col-sm-offset-2'><div class='panel'><div class='panel-body'><h3>"+algo[i]+"</h3><table class='table table-striped'><thead><tr><th>Process ID</th><th>Burst time</th><th>Arrival time</th><th>Waiting Time</th><th>Turn Around Time</th><th>Completion time</th></tr></thead><tbody class='table-body"+i+"'></tbody></table></div></div></div></div>");
+        var cur = output[i].split('\n');
+        console.log(cur);
+        for(var j = 0; j < cur.length-2; ++j) {
+            var row = cur[j].split('\t');
+            console.log(row);
+            $(".table-body"+i).append("<tr>" +
+                "<td>" + row[0]+ "</td>" +
+                "<td>" + row[1]+ "</td>" +
+                "<td>" + row[2]+ "</td>" +
+                "<td>" + row[3]+ "</td>" +
+                "<td>" + row[4]+ "</td>" +
+                "<td>" + row[5]+ "</td>" +
+                "</tr>");
+        }
+    }
+}
--- routes/executables/cpu_scheduling/fcfs.c
+++ routes/executables/cpu_scheduling/fcfs.c
@@ -44,11 +44,9 @@ int main(int argc, char const *argv[])
 	{
 		total_waiting_time+=p[i].waiting_time;
 		total_turn_around_time+=p[i].turn_around_time;
-
 		int completion_time=p[i].turn_around_time + p[i].arrival_time;
-
 		printf("%d\t%d\t%d\t%d\t%d\t%d\n",p[i].process_id,p[i].burst_time, p[i].arrival_time, p[i].waiting_time,p[i].turn_around_time,completion_time);
 	}
-	printf("\n%f\n", (float)total_waiting_time/n);
-	printf("\n%f\n",(float)total_turn_around_time/n);
+	printf("%f\n", (float)total_waiting_time/n);
+	printf("%f",(float)total_turn_around_time/n);
 }
