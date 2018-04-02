e46c6d47417b8223168dd1460f15633eef484c34
Ashrit26 <ashritmr@rocketmail.com>
2018-04-02 14:18:56 +0530

    Add files via upload

---
+++ routes/executables/system_calls/getid.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+int main()
+{
+	uid_t uid = getuid();
+	uid_t euid = geteuid();
+	if(uid==0 && euid==0)
+		printf("Welcome root user\n");
+	else
+		printf("You aren't root\nuid=%d euid=%d\n",uid,euid);
+
+	gid_t gid = getgid();
+	gid_t egid = getegid();
+	printf("gid=%d egid=%d\n",gid,egid );
+
+	pid_t pid = getpid();
+	pid_t ppid = getppid();
+	printf("\npid=%d ppid=%d\n",pid,ppid);
+}
