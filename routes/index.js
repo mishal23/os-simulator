8456e5b754f487c7b5af0d3e7196380db151e670
Mishal Shah <shahmishal1998@gmail.com>
2020-10-15 15:22:21 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,6 +15,13 @@ router.get('/testslackbot', function(req, res, next) {
 router.post('/slackbot', function(req, res, next) {
 	console.log(req.body);
 	res.send(req.body.challenge);
+});
+
+router.post('/upload_file', function(req, res, next) {
+	fs.writeFile('./public/test.txt', "hi testing", function (err) {
+	  if (err) throw err;
+	  res.send("saved file");
+	}); 
 });
 
 module.exports = router;
